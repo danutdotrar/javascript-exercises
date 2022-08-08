cabdf889efc61edbc7161f25ebcabf308c122f6f
danutdotrar <morarasu.danut@gmail.com>
2022-08-08 17:01:12 +0300

    Solved 4th  exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    const arrays = args[0];
 
+    const newArray = [];
+
+    array.forEach((item) => {
+        if (!args.include(item)) {
+            newArray.push(item);
+        }
+    });
+
+    return newArray;
 };
 
 // Do not edit below this line
